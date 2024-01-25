69f97dd6fc31eac5555f399fd0e883783cbdf214
suyash002 <getontosuyash@gmail.com>
2024-01-25 12:26:45 +0530

    error solve

--- src/screens/Home/Features/Features.js
+++ src/screens/Home/Features/Features.js
@@ -10,114 +10,110 @@ import nails from "../../../assets/FeatureImages/nails.jpg";
 import bodyMassage from "../../../assets/FeatureImages/bodyMassage.jpg";
 import threading from "../../../assets/FeatureImages/threading.jpg";
 
-
-
 const Features = () => {
   return (
     <div>
       {/* Featured Categories */}
       <section className="section-padding">
-      <div className="container">
-        <div className="featured-category">
-          <div>
-          <h2 className="main-heading">Featured Categories</h2>
-          </div>
-
-          <div className="row">
-            <div className="col-lg-3 col-md-6 col-sm-6">
-           
-              <div className="category-box">
-              <Link to="/FeatureSubCat">
-                <div className="feature-icon">
-                  <span className="feature-background">
-                    <img src={facialImg} className="feature-logo"></img>
-                  </span>
-                  <h5 className="feature-headings">Facial & Cleanup</h5>
-                </div>
-                </Link>
-              </div>
-              
+        <div className="container">
+          <div className="featured-category">
+            <div>
+              <h2 className="main-heading">Featured Categories</h2>
             </div>
 
-            <div className="col-lg-3 col-md-6 col-sm-6">
-              <div className="category-box">
-              <div className="feature-icon">
-                  <span className="feature-background">
-                    <img src={dTanImage} className="feature-logo"></img>
-                  </span>
-                  <h5>Bleach & D-Tan</h5>
+            <div className="row">
+              <div className="col-lg-3 col-md-6 col-sm-6">
+                <div className="category-box">
+                  <Link to="/FeatureSubCat">
+                    <div className="feature-icon">
+                      <span className="feature-background">
+                        <img src={facialImg} className="feature-logo"></img>
+                      </span>
+                      <h5 className="feature-headings">Facial & Cleanup</h5>
+                    </div>
+                  </Link>
                 </div>
               </div>
-            </div>
 
-            <div className="col-lg-3 col-md-6 col-sm-6">
-              <div className="category-box">
-              <div className="feature-icon">
-                  <span className="feature-background">
-                    <img src={ManiPadicure} className="feature-logo"></img>
-                  </span>
-                  <h5>Mani-Padi</h5>
+              <div className="col-lg-3 col-md-6 col-sm-6">
+                <div className="category-box">
+                  <div className="feature-icon">
+                    <span className="feature-background">
+                      <img src={dTanImage} className="feature-logo"></img>
+                    </span>
+                    <h5>Bleach & D-Tan</h5>
+                  </div>
                 </div>
               </div>
-            </div>
 
-            <div className="col-lg-3 col-md-6 col-sm-6">
-              <div className="category-box">
-              <div className="feature-icon">
-                  <span className="feature-background">
-                    <img src={HairCare} className="feature-logo"></img>
-                  </span>
-                  <h5>Hair Care</h5>
+              <div className="col-lg-3 col-md-6 col-sm-6">
+                <div className="category-box">
+                  <div className="feature-icon">
+                    <span className="feature-background">
+                      <img src={ManiPadicure} className="feature-logo"></img>
+                    </span>
+                    <h5>Mani-Padi</h5>
+                  </div>
                 </div>
               </div>
-            </div>
 
-            <div className="col-lg-3 col-md-6 col-sm-6">
-              <div className="category-box">
-              <div className="feature-icon">
-                  <span className="feature-background">
-                    <img src={preBridal} className="feature-logo"></img>
-                  </span>
-                  <h5>Pre-Bridal</h5>
+              <div className="col-lg-3 col-md-6 col-sm-6">
+                <div className="category-box">
+                  <div className="feature-icon">
+                    <span className="feature-background">
+                      <img src={HairCare} className="feature-logo"></img>
+                    </span>
+                    <h5>Hair Care</h5>
+                  </div>
                 </div>
               </div>
-            </div>
 
-            <div className="col-lg-3 col-md-6 col-sm-6">
-              <div className="category-box">
-              <div className="feature-icon">
-                  <span className="feature-background">
-                    <img src={threading} className="feature-logo"></img>
-                  </span>
-                  <h5>Threading</h5>
+              <div className="col-lg-3 col-md-6 col-sm-6">
+                <div className="category-box">
+                  <div className="feature-icon">
+                    <span className="feature-background">
+                      <img src={preBridal} className="feature-logo"></img>
+                    </span>
+                    <h5>Pre-Bridal</h5>
+                  </div>
                 </div>
               </div>
-            </div>
 
-            <div className="col-lg-3 col-md-6 col-sm-6">
-              <div className="category-box">
-              <div className="feature-icon">
-                  <span className="feature-background">
-                    <img src={nails} className="feature-logo"></img>
-                  </span>
-                  <h5>Nails</h5>
+              <div className="col-lg-3 col-md-6 col-sm-6">
+                <div className="category-box">
+                  <div className="feature-icon">
+                    <span className="feature-background">
+                      <img src={threading} className="feature-logo"></img>
+                    </span>
+                    <h5>Threading</h5>
+                  </div>
                 </div>
               </div>
-            </div>
 
-            <div className="col-lg-3 col-md-6 col-sm-6">
-              <div className="category-box">
-              <div className="feature-icon">
-                  <span className="feature-background">
-                    <img src={bodyMassage} className="feature-logo"></img>
-                  </span>
-                  <h5>Body Deals</h5>
+              <div className="col-lg-3 col-md-6 col-sm-6">
+                <div className="category-box">
+                  <div className="feature-icon">
+                    <span className="feature-background">
+                      <img src={nails} className="feature-logo"></img>
+                    </span>
+                    <h5>Nails</h5>
+                  </div>
+                </div>
+              </div>
+
+              <div className="col-lg-3 col-md-6 col-sm-6">
+                <div className="category-box">
+                  <div className="feature-icon">
+                    <span className="feature-background">
+                      <img src={bodyMassage} className="feature-logo"></img>
+                    </span>
+                    <h5>Body Deals</h5>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
       </section>
     </div>
   );
